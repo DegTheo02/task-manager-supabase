8371eb724c1ddf44aa7f5e35ddc0748e576c86d5
DegTheo02 <39355441+DegTheo02@users.noreply.github.com>
2025-12-13 02:59:58 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import Dashboard from "./Dashboard";
-import Kanban from "./Kanban";
 import Tasks from "./Tasks";
+import Kanban from "./Kanban";
 
 export default function App() {
   return (
-    <Router>
+    <>
       <Navbar />
-      <div style={{ padding: "20px" }}>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/kanban" element={<Kanban />} />
-          <Route path="/tasks" element={<Tasks />} />
-        </Routes>
-      </div>
-    </Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/kanban" element={<Kanban />} />
+      </Routes>
+    </>
   );
 }
