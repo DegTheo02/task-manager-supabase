ba375fa08e904cad92d8bc56d15e29eff771f599
DegTheo02 <39355441+DegTheo02@users.noreply.github.com>
2026-03-14 14:44:27 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,6 +3,13 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true
+  }
+});
+
 if (!supabaseUrl) {
   throw new Error("VITE_SUPABASE_URL is missing in your .env file");
 }
